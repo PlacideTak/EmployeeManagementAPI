94e26dc94749d228a736d23a1e51fa8ea6e9d13c
Placidetak <placidetak@gmail.com>
2026-03-25 15:28:17 -0400

    Correction RoleHelper

--- WebApplication1/Helpers/IRoleHelper.cs
+++ WebApplication1/Helpers/IRoleHelper.cs
@@ -1,6 +1,13 @@
-﻿namespace WebApplication1.Helpers
+﻿using System.Security.Claims;
+
+namespace WebApplication1.Helpers
 {
     public interface IRoleHelper
     {
+        bool IsAdmin(ClaimsPrincipal user);
+        bool IsManager(ClaimsPrincipal user);
+        bool IsEmployee(ClaimsPrincipal user);
+        bool HasRole(ClaimsPrincipal user, string role);
+        void RequireRole(ClaimsPrincipal user, string role);
     }
 }
